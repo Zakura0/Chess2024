e685563c39f5a6710b90268208730f1554360ec6
skazemi <simon.kazemi@mcs.de>
2024-06-05 15:51:48 +0200

    board und move

--- src/Board.java
+++ src/Board.java
@@ -1,17 +1,43 @@
 public class Board {
     
-    public static Piece [][] board;
+    public Piece[][] board;
 
     public  Board(){
         board = new Piece[8][8];
     }
-    /*
-     * Using FEN to initialize the board
-     */
+
+    public Piece[][] getBoard() {
+        return board;
+    }
+
     public void initializeBoard() {
-        
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(1, i, true);
+            board[6][i] = new Pawn(6, i, false);
+        }
+        board[0][0] = new Rook(0, 0, true);
+        board[0][1] = new Knight(0, 1, true);
+        board[0][2] = new Bishop(0, 2, true);
+        board[0][3] = new Queen(0, 3, true);
+        board[0][4] = new King(0, 4, true);
+        board[0][5] = new Bishop(0, 5, true);
+        board[0][6] = new Knight(0, 6, true);
+        board[0][7] = new Rook(0, 7, true);
+        board[7][0] = new Rook(7, 0, false);
+        board[7][1] = new Knight(7, 1, false);
+        board[7][2] = new Bishop(7, 2, false);
+        board[7][3] = new Queen(7, 3, false);
+        board[7][4] = new King(7, 4, false);
+        board[7][5] = new Bishop(7, 5, false);
+        board[7][6] = new Knight(7, 6, false);
+        board[7][7] = new Rook(7, 7, false);
+
 
     }
+
+    
+
+    
 
 
 }
--- src/Move.java
+++ src/Move.java
@@ -1,19 +1,31 @@
 public class Move {
 
-    private int _curr;
-    private int _dest;
+    private int _curr_row;
+    private int _curr_col;
+    private int _dest_row;
+    private int _dest_col;
 
-    public Move(int curr, int dest) {
-        _curr = curr;
-        _dest = dest;
+    public Move(int curr_row, int curr_col, int dest_row, int dest_col) {
+        _curr_row = curr_row;
+        _curr_col = curr_col;
+        _dest_row = dest_row;
+        _dest_col = dest_col;
     }
 
-    public int getCurr() {
-        return _curr;
+    public int getCurrRow() {
+        return _curr_row;
     }
 
-    public int getDest() {
-        return _dest;
+    public int getCurrCol() {
+        return _curr_col;
+    }
+
+    public int getDestRow() {
+        return _dest_row;
+    }
+
+    public int getDestCol() {
+        return _dest_col;
     }
     
 }
