c9f7f1c70cdc3ba54f17e399ac6f402cdcb854e3
artanishaiilo <ben.woller@haiilo.com>
2024-07-03 13:58:01 +0200

    set transforming pawn

--- src/Game.java
+++ src/Game.java
@@ -316,10 +316,9 @@ public class Game {
 
 
     private void checkTransform(Pawn piece) {
-        if (piece.getRow() == 0) {
+        if (piece.getRow() == 0 || piece.getRow() == 7) {
+            transformingPawn = piece;
             boardGUI.showTransform(piece);  // Weiss, da nur diese Pawns zu Rank 0 kommen können, 1 aus testgründen
-        } else if (piece.getRow() == 7) {
-            boardGUI.showTransform(piece); // Schwarz, da nur diese Pawns zu Rank 7 kommen können, 6 aud testgründen
         }
     }
 
